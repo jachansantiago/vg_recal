4f38b6db41777d51cc28afa1810ac356e24d5f38
binarySequoia <jeffrey.chan@upr.edu>
2018-08-14 13:23:50 -0700

    add utils files

---
+++ json2csv.py
@@ -0,0 +1,16 @@
+"""
+This script convert train data from json format to csv format.
+
+"""
+
+from load_data import *
+import os
+
+DIR = "data/train_gamcompare/json/"
+CSV_DIR = "data/train_gamcompare/csv/"
+train_files = os.listdir(DIR)
+
+for file in train_files:
+    name = file[:-5]
+    df = json2csv(os.path.join(DIR, file), debug=False, bow=True)
+    df.to_csv(os.path.join(CSV_DIR, name+".csv"), index=False)
---
+++ json2csv_t.py
@@ -0,0 +1,16 @@
+"""
+This script convert test data from json format to csv format.
+
+"""
+
+from load_data import *
+import os
+
+DIR = "data/test_gamcompare/json/"
+CSV_DIR = "data/test_gamcompare/csv/"
+test_files = os.listdir(DIR)
+
+for file in test_files:
+    name = file[:-5]
+    df = json2csv(os.path.join(DIR, file), debug=False, bow=True)
+    df.to_csv(os.path.join(CSV_DIR, name+".csv"), index=False)
